3ea246bd207d49395fac6b2d15e9d6ccc00dedd2
joaoselim <finick117@gmail.com>
2026-02-24 14:44:04 -0300

    Add files via upload

---
+++ ex4/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Entre com o numero de alunos: ");
+
+        int numAlunos = entrada.nextInt();
+
+        switch (numAlunos){
+            case 10,20:
+                System.out.println("Sala I-16");
+                break;
+            case 30:
+                System.out.println("Sala I-22");
+                break;
+        }
+    }
+}
