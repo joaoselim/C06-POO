6af4914b1f68fe9850338b9635ffa62efc6f47ec
joaoselim <finick117@gmail.com>
2026-06-02 12:14:54 -0300

    Initialize JFrame for Bob Game
    criando a main

--- Bob_Escape_Room/src/Main.java
+++ Bob_Escape_Room/src/Main.java
@@ -1,5 +1,24 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
 
+        JFrame window = new JFrame();
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Bob Game");
+
+        PainelJogo painelJogo = new PainelJogo();
+
+        window.add(painelJogo);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        painelJogo.startGameThread();
+        
     }
 }
