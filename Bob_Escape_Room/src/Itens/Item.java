6ea1a681df5f560ec23e7f92e14cfeac2e220040
IguKazu <igor@mavigno.com>
2026-06-02 20:43:58 -0300

    parte do inventario feito

--- Bob_Escape_Room/src/Itens/Item.java
+++ Bob_Escape_Room/src/Itens/Item.java
@@ -2,4 +2,19 @@ package Itens;
 
 public class Item {
 
+    private String nome;
+    private String descricao;
+
+    public Item(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
 }
